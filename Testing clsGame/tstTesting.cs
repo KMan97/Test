5ac00e26009e45d54a60ac876ed0bb2ed8d37f1b
p17155191 <p17155191@GWHXX581WIN004W>
2019-02-04 11:29:35 +0000

    Creation of a new test class called "tstTesting"

---
+++ Testing clsGame/tstTesting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing_clsGame
+{
+    [TestClass]
+    public class tstTesting
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class
+            clsTesting ATest = new clsTesting();
+            //check to see if the class is not null
+            Assert.IsNotNull(ATest);
+        }
+    }
+}
